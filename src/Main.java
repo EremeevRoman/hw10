c3e94216dc388c477d1966534f9b1935b47c4e07
Roman <romaneremeev7@mail.ru>
2023-03-17 20:44:14 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanov ";
+        String lastName = "Ivanovich ";
+        String fullName = middleName + firstName + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String firstName = "Ivan ";
+        String middleName = "Ivanov ";
+        String lastName = "Ivanovich ";
+        String fullName = middleName + firstName + lastName;
+        String small = fullName;
+        System.out.println(small.toUpperCase());
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String firstName = "Иванов ";
+        String middleName = "Семён ";
+        String lastName = "Семёнович ";
+        String fullName = middleName + firstName + lastName;
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+
+
     }
 }
